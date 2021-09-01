125f6e3c2a87ea6653502c839979006aaf0b99ad
Chetan Zawar <chetan.zawar@tietoevry.com>
2021-09-01 16:01:24 +0530

    period impl

--- salesubscription-service/src/main/java/com/salesubscription/service/salesubscriptionservice/service/impl/SubscriptionImpl.java
+++ salesubscription-service/src/main/java/com/salesubscription/service/salesubscriptionservice/service/impl/SubscriptionImpl.java
@@ -4,12 +4,15 @@
 package com.salesubscription.service.salesubscriptionservice.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salesubscription.service.salesubscriptionservice.dao.SubscriptionDao;
 import com.salesubscription.service.salesubscriptionservice.domain.Subscription;
+
+
 
 /**
  * @author mahanraj
@@ -25,5 +28,11 @@ public class SubscriptionImpl {
 		List<Subscription> subscriptions = subscriptionDao.findAll();
 		return subscriptions;
 	}
-
+	
+	public Subscription getSubscriptionById(Integer subscriptionId){
+		Optional<Subscription> subscription = subscriptionDao.findById(subscriptionId);
+		return subscription.get();
+	}
+	
 }
+
