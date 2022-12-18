cc37cdda41bbb11ce15636232dbb44a854a2792f
raj33 <raj.33jsr@gmail.com>
2022-12-18 10:42:48 +0530

    resolved conflict

--- salesubscription-service/src/main/java/com/salesubscription/service/salesubscriptionservice/rest/PeriodRestful.java
+++ salesubscription-service/src/main/java/com/salesubscription/service/salesubscriptionservice/rest/PeriodRestful.java
@@ -1,11 +1,9 @@
 package com.salesubscription.service.salesubscriptionservice.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
->>>>>>> b30674e7e38e26421c22329441168b65ac5c393e
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,18 +32,12 @@ public class PeriodRestful {
 	}
 	
 	@PostMapping("")
-<<<<<<< HEAD
-	public void savePeriod(@RequestBody Period period) {
-		Subscription sub = subsImpl.getSubscriptionById(period.getSubscriptionId());
-		period.setSubscription(sub);
-		periodImpl.savePeriod(period);
-=======
+
 	public ResponseEntity<Period> savePeriod(@RequestBody Period period) {
 		Subscription sub = subsImpl.getSubscriptionById(period.getSubscriptionId());
 		period.setSubscription(sub);
 		Period periodResp = periodImpl.savePeriod(period);
 		return new ResponseEntity<Period>(periodResp,HttpStatus.CREATED);
->>>>>>> b30674e7e38e26421c22329441168b65ac5c393e
 	}
 	
 	
